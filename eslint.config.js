2d7755bc441f4fe501aeff0429b508a7e40940cc
Nech-C <xiaokunchen@outlook.com>
2025-08-09 15:58:40 -0400

    chore(ci): add EsLint

--- eslint.config.js
+++ eslint.config.js
@@ -1,29 +1,11 @@
 import js from '@eslint/js'
 import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import pluginReact from 'eslint-plugin-react'
+import { defineConfig } from 'eslint/config'
+import stylistic from '@stylistic/eslint-plugin'
 
 export default defineConfig([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
-    },
-    rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-    },
-  },
+  { ignores: ['package.json', 'dist/**'] },
+  { files: ['**/*.{js,mjs,cjs,jsx}'], plugins: { js, stylistic }, extends: ['js/recommended', 'stylistic/recommended'], languageOptions: { globals: globals.browser } },
+  pluginReact.configs.flat.recommended,
 ])
